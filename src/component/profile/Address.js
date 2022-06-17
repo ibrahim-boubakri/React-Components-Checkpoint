b134e720eb0c98fdb4988907bac0f8ddc70e2a17
ibrahim-boubakri <107005643+ibrahim-boubakri@users.noreply.github.com>
2022-06-17 01:23:06 +0100

    first commit

---
+++ src/component/profile/Address.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Address = () => {
+ return <h2> 36 Avenue Saudi Arabia, 2073, Borj Louzir Ariana Tunisia </h2>;
+};
+export default Address;
---
+++ src/component/profile/ProfilePhoto.js
@@ -0,0 +1,3 @@
+import React from "react";
+const ProfilePhoto = () => { return <img src="https://media-exp1.licdn.com/dms/image/C4E03AQEhytygW3CXDg/profile-displayphoto-shrink_200_200/0/1626390762572?e=1658361600&v=beta&t=L-K46OXdV5qSDYCiHVXMF_lMw8AKq3zVUVojD6Nq1oE" alt="description"></img>};
+export default ProfilePhoto;
